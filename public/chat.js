4d5e110d0a5547ceb250b5ef1bab5df76ae8b02a
Daniel Vadil <dvadz@yahoo.com>
2019-08-21 01:14:25 -0400

    - added DOM element handlers - added Send button listener that sends out new chat message - added chat listener that displays new incoming chat messages

--- public/chat.js
+++ public/chat.js
@@ -1,2 +1,21 @@
 // Make socket connection
 const socket = io.connect("localhost:4000");
+
+// Query DOM
+const message = document.getElementById("message");
+const handle = document.getElementById("handle");
+const btn = document.getElementById("send");
+const output = document.getElementById("output");
+
+// Event listeners
+btn.addEventListener("click", function() {
+  socket.emit("chat", {
+    message: message.value,
+    handle: handle.value
+  });
+});
+
+// Listen for events
+socket.on("chat", function(data) {
+  output.innerHTML += "<p><strong>" + data.handle + "</strong>" + data.message + "</p>";
+});
